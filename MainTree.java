62c8634c6804d462db30657a2e75681f49b57cc8
pranav21999 <pranavdani99@gmail.com>
2021-07-04 13:15:01 +0530

    Added method for Insertion of nodes

--- MainTree.java
+++ MainTree.java
@@ -1,24 +1,25 @@
 package binarytree;
 
-
 public class MainTree {
 	public static void main(String[] args) {
 		//creating instances
 		BinaryTree bst = new BinaryTree();
 		Node root = null;
-		/*Inserting node in the Binary tree
-			56->root nood
-			30 ->left node
-			70->right node	*/
-		root = bst.insert(root, 56);
+		/*Inserting node in the Binary tree*/
 		
+		root = bst.insert(root, 56);
 		root = bst.insert(root, 30);
-	
 		root = bst.insert(root, 70);
-		root = bst.insert(root, 46);
-
+		root = bst.insert(root, 22);
+		root = bst.insert(root, 40);
+		root = bst.insert(root, 60);
+		root = bst.insert(root, 95);
+		root = bst.insert(root, 11);
+		root = bst.insert(root, 65);
+		root = bst.insert(root, 3);
+		root = bst.insert(root, 6);
+		root = bst.insert(root, 63);
+		root = bst.insert(root, 67);
 		bst.print(root);
-
 	}
-
 }
